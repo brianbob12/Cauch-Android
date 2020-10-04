57bb835eacfe3c8cf2e6704ab834e193e45bf4d6
Cyrus Signer <japaneserhino@gmail.com>
2020-10-04 13:43:04 +0100

    Icon

---
+++ app/src/main/java/com/example/scheduleapp/DayList.kt
@@ -0,0 +1,16 @@
+package com.example.scheduleapp
+
+import java.sql.Date
+
+class DayList {
+    val date:Date
+
+    override fun toString(): String {
+        
+        return super.toString()
+    }
+
+    constructor(date: Date){
+        this.date =date
+    }
+}
