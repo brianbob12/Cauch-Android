df81fad01f93bd6bc1d92b9aa9204e5081d05512
Cyrus Signer <japaneserhino@gmail.com>
2020-10-05 18:00:24 +0100

    Fixing broken task layout and setting up TaskTags

---
+++ app/src/main/java/com/example/scheduleapp/TaskTag.kt
@@ -0,0 +1,14 @@
+package com.example.scheduleapp
+
+import android.graphics.Color
+
+class TaskTag {
+    private var name:String
+    private var color:Color
+
+    constructor(name:String, color:Color){
+        this.name=name
+        this.color=color
+    }
+
+}
